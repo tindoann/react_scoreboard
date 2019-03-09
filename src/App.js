920d7b56f7023762957bc38e41cd158ca5be3508
TinX <TinX@TinX.local>
2019-03-08 19:35:34 -0500

    fix add button and css responsive

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+
 import React, { Component } from 'react'; 
 import './App.css'; 
 import Team from './Team'; 
 
-class App extends Component { 
+// This page sets up the header and initial 'teamName' section 
+
+class App extends Component {
   render() {
     let teams = [
       {
-        teamName: 'Team A', 
-        initialScore: 0 
-      }, 
+        teamName: 'Team A',
+        initialScore: 0
+      },
       {
-        teamName: 'Team B', 
-        initial: 0
+        teamName: 'Team B',
+        initialScore: 0
       }
     ]
     return (
-      <div> 
+      <div>
         <h1>My Scoreboard</h1>
         <main>
           {teams.map((team, index) => (
             <Team
-            key={index}
-            teamName={team.teamName}
-            initialScore={team.initialScore}
+              key={index}
+              teamName={team.teamName}
+              initialScore={team.initialScore}
             />
           ))}
         </main>
--- src/Team.js
+++ src/Team.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { inlineArrayTransformer } from 'common-tags'
+// This sets up the 'Update Team Name' and 'Update Team Score' components 
+// the constructor set the initial state that loads at the beginning
+// set the super(props) gives us the ability to use props throughout 
 
 class Team extends Component {
   constructor(props) {
@@ -11,11 +14,15 @@ class Team extends Component {
     }
   }
 
+// ability to change the value of the state
+
   updateTeamName = event => { 
     this.setState({
       teamName: event.target.value
     })
   }
+
+  // increment the score 
 
   addOneToScore = () => {
     this.setState({
@@ -23,6 +30,7 @@ class Team extends Component {
     })
   }
  
+  // decreases the score 
   subtractOneFromScore = () => {
     this.setState({
       score: this.state.score - 1
@@ -32,16 +40,16 @@ class Team extends Component {
   render() {
     return (
       <section>
-        <h2 className='name'>{this.state.teamName}</h2>
+        <h2 className="name">{this.state.teamName}</h2>
         <p>{this.state.score}</p>
         <ul>
           <li>
             <label>Update Team Name</label>
-            <input type="text" value={this.state.teamNames} onChange={this.updateTeamName} />
+            <input type="text" value={this.state.teamName} onChange={this.updateTeamName} />
           </li>
           <li>
             <label>Update Team Score</label>
-            <button onClick={this.addOnetoScore}>Add 1</button>
+            <button onClick={this.addOneToScore}>Add 1</button>
             <button onClick={this.subtractOneFromScore}>Subtract 1</button>
           </li>
         </ul>
